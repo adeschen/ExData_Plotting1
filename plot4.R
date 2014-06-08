dd45b3670e841e51f535e820eda96ef03d5a52ea
adeschen <adeschen@hotmail.com>
2014-06-08 01:23:09 -0400

    Add plot 4 code.

---
+++ plot4.R
@@ -0,0 +1,67 @@
+########################################################################
+# Create histogram of the Global Active Power 
+########################################################################
+
+# Load household power consumption file
+# Missing values are coded as ?
+data<-read.table("household_power_consumption.txt", header=T, sep=";", na.strings="?")
+
+# Paste Date and Time to create a Time class column
+data$Date_Time<-strptime(paste(data$Date, data$Time), format='%d/%m/%Y %H:%M:%S')
+
+# Transform Date field into Date class
+data$Date<-as.Date(data$Date,format='%d/%m/%Y')
+
+# Create list of 2 dates used for to create the graph : 2007-02-01 and 2007-02-02
+dateList<-c(as.Date("2007-02-01"), as.Date("2007-02-02"))
+
+
+# Create dataset containing only the information for the specified list of 2 dates
+dataSubset<-data[data$Date %in% dateList,]
+
+# Open device to create PNG file
+png(filename = "plot4.png",
+    width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "transparent",  res = NA)
+
+# 2 by 2 plots in one file
+par(mfrow=c(2,2))
+
+# Create plot of Global Active Power in time 
+plot(dataSubset$Date_Time, dataSubset$Global_active_power, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="", main="")
+
+# Create plot of Voltage in time 
+plot(dataSubset$Date_Time, dataSubset$Voltage, type="l",
+     ylab="Voltage", xlab="datetime", main="")
+
+# Create plot of Submetering 1 in time 
+# Fix y limits to ensure that all 3 curves use lthe same limits
+plot(dataSubset$Date_Time, dataSubset$Sub_metering_1, type="l", 
+     col=c("black"), ylim=c(0,38),
+     ylab="Energy sub metering", xlab="", main="")
+
+par(new=T)
+
+# Create plot of Submetering 2 in time 
+# Fix y limits to ensure that all 3 curves use lthe same limits
+plot(dataSubset$Date_Time, dataSubset$Sub_metering_2, type="l", col=c("red"),
+     ylim=c(0,38), ylab="", xlab="", main="")
+
+par(new=T)
+
+# Create plot of Submetering 3 in time 
+# Fix y limits to ensure that all 3 curves use lthe same limits
+plot(dataSubset$Date_Time, dataSubset$Sub_metering_3, type="l", col=c("blue"),
+     ylim=c(0,38), ylab="", xlab="", main="")
+
+# Add legend
+legend('topright', c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=c(1,1), 
+       lwd=c(2.5,2.5),col=c("black","red", "blue"))
+
+# Create plot of Global Active Power in time 
+plot(dataSubset$Date_Time, dataSubset$Global_reactive_power, type="l",
+     ylab="Global_reactive_power", xlab="datetime", main="")
+
+# Close device
+dev.off()
